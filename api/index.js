5d02383e4bac5750d14a25b0b96e91b3e5e61974
venkateshmadari <venkateshmadari07@gmail.com>
2024-03-16 20:34:58 +0530

    connected to databse and installed bcryptjs extension to decrypt the password

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO ).then(() => {
@@ -10,7 +11,9 @@ mongoose.connect(process.env.MONGO ).then(() => {
     console.log(err);
 })
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
     console.log("the server is running successfully in 3000");
 });
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
